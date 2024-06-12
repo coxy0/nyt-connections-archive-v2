5255a54c39676a9148c4953d4c7e79d1b4407fa4
coxy0 <olicox2005@gmail.com>
2024-06-12 04:02:55 +0100

    Added help modal to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { getDataForDate } from "./utils/data";
+import { useState } from "react";
 import data from "./assets/connections.json";
 import Game from "./components/Game/Game";
+import HelpModal from "./components/Modal/HelpModal";
+import { getDataForDate } from "./utils/data";
 
 const App = () => {
+  const [showHelp, setShowHelp] = useState(true);
+
   const date = "2023-06-18";
   const dateData = getDataForDate(data, date);
   const gameData = dateData?.answers || [];
-  // console.log(gameData);
   const words = gameData?.flatMap((group) => group.members) || [];
 
   return (
     <>
+      <HelpModal visible={showHelp} onClickClose={() => setShowHelp(false)} />
       <Game gameData={gameData} initialWords={words} />
     </>
   );
